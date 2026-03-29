01a3bb2367b71098aadda642a231f349feab1b69
Rogerio Almeida <rogerioja89@gmail.com>
2026-03-29 11:56:58 -0300

    Projeto Criado

---
+++ src/main/java/com/dominio/maxvendas/MaxvendasApplication.java
@@ -0,0 +1,13 @@
+package com.dominio.maxvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxvendasApplication.class, args);
+	}
+
+}

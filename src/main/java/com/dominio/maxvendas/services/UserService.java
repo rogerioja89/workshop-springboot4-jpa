b31605737971e8a6fd00f26c51aa9de89f171b7d
Rogerio Almeida <rogerioja89@gmail.com>
2026-03-31 23:52:22 -0300

    Service layer, component registration

---
+++ src/main/java/com/dominio/maxvendas/services/UserService.java
@@ -0,0 +1,26 @@
+package com.dominio.maxvendas.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dominio.maxvendas.entities.User;
+import com.dominio.maxvendas.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}

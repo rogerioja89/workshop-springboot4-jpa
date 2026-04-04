9c5f0f2a20182b5abc7ab145ee8bb30ee49a1516
Rogerio Almeida <rogerioja89@gmail.com>
2026-04-03 21:58:50 -0300

    Exception handling - findById

--- src/main/java/com/dominio/maxvendas/services/UserService.java
+++ src/main/java/com/dominio/maxvendas/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.dominio.maxvendas.entities.User;
 import com.dominio.maxvendas.repositories.UserRepository;
+import com.dominio.maxvendas.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {

7411540e76a8185f2de5497e9528715cc665b3f5
Rogerio Almeida <rogerioja89@gmail.com>
2026-04-03 22:28:45 -0300

    Exception handling - delete

--- src/main/java/com/dominio/maxvendas/services/UserService.java
+++ src/main/java/com/dominio/maxvendas/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.dominio.maxvendas.entities.User;
 import com.dominio.maxvendas.repositories.UserRepository;
+import com.dominio.maxvendas.services.exceptions.DatabaseException;
 import com.dominio.maxvendas.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
@@ -44,4 +53,4 @@ public class UserService {
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
 	}
-} 	
+}
